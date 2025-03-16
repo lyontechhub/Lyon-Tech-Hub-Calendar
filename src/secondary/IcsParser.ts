d6fa009145020ecc3f729afb4dced2e45b796940
fpellet <git@florentpellet.com>
2025-03-16 21:26:51 +0100

    Fix timezone bug

--- src/secondary/IcsParser.ts
+++ src/secondary/IcsParser.ts
@@ -70,12 +70,17 @@ const parseEvent = (limitMax: Date) => (event: VEvent): Event => {
     const fixDate = (date: Date) => {
       // Fix to correct the time according to the timezone documented in the libs readme. The question is why doesn't the libs do this directly??
       if (rrule.origOptions.tzid) {
+        const localTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
         const tz = rrule.origOptions.tzid
+
+        const localComputeTimezoneOffset = getTimezoneOffset(localTimezone, date);
         const computeOffset = getTimezoneOffset(tz, date);
         const originOffset = getTimezoneOffset(tz, event.start);
 
-        const deltaOffset = computeOffset - originOffset;
+        const deltaOffsetTimezoneError = computeOffset - localComputeTimezoneOffset
+        const deltaOffsetOfOrigin = computeOffset - originOffset
 
+        const deltaOffset = deltaOffsetOfOrigin - deltaOffsetTimezoneError
         return dateFns.addMilliseconds(date, -deltaOffset)
       } else {
         return dateFns.addHours(date, date.getHours() - ((event.start.getTimezoneOffset() - date.getTimezoneOffset()) / 60))
