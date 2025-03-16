42384f77babee7814855595042cf9b5e346ba177
fpellet <git@florentpellet.com>
2025-03-16 22:12:50 +0100

    style

--- src/secondary/IcsCalendarEvent.ts
+++ src/secondary/IcsCalendarEvent.ts
@@ -1,9 +1,11 @@
-import { Event, EventData } from './IcsParser';
-import { CalendarEvent, Interval, DateOnly } from '../domain/CalendarEvent';
-import { Name } from '../domain/Name';
 import * as dateFns from 'date-fns';
 
-const convertToCalendarEvent = (group: string)=> (data: EventData) => (id: string) => (date: Interval) => {
+import { CalendarEvent, Interval, DateOnly } from '../domain/CalendarEvent';
+import { Name } from '../domain/Name';
+
+import { Event, EventData } from './IcsParser';
+
+const convertToCalendarEvent = (group: string) => (data: EventData) => (id: string) => (date: Interval) => {
   return CalendarEvent.of({
     id: `${group}-${id}`,
     title: Name.of(data.title),
@@ -13,28 +15,24 @@ const convertToCalendarEvent = (group: string)=> (data: EventData) => (id: strin
     address: data.location || undefined,
     geo: data.geo || undefined,
     url: data.url || undefined,
-  })
-}
+  });
+};
 
 export const toCalendarEvents =
   (group: string) =>
   (event: Event): CalendarEvent[] => {
-    if(event.type == 'single') {
-      return [
-        convertToCalendarEvent(group)(event.data)(event.id)(event.date)
-      ];
-    }
-    else {
-      const convert = convertToCalendarEvent(group)(event.data)
+    if (event.type == 'single') {
+      return [convertToCalendarEvent(group)(event.data)(event.id)(event.date)];
+    } else {
+      const convert = convertToCalendarEvent(group)(event.data);
       const formatKey = (date: DateOnly | Date) => {
-        if(date instanceof Date) {
-          return dateFns.formatDate(date, 'yyyy-MM-dd')
+        if (date instanceof Date) {
+          return dateFns.formatDate(date, 'yyyy-MM-dd');
         }
-        return `${date.year}-${date.month}-${date.day}`
-      }
-      return event.dates.map(date => {
-        return convert(`${event.id}-${formatKey(date.start)}`)(date)
-      })
+        return `${date.year}-${date.month}-${date.day}`;
+      };
+      return event.dates.map((date) => {
+        return convert(`${event.id}-${formatKey(date.start)}`)(date);
+      });
     }
-  }
-
+  };
